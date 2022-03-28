8aad84e42416f069dd1da16f274fb1bef04adff8
Muhammad Ahmer Ghouri <ahmer7740@jmc.edu>
2022-03-28 17:44:23 +0500

    Using for loop making tabs and its content

---
+++ news.js
@@ -0,0 +1,681 @@
+
+let responseText = [
+    {
+        "area": "events",
+        "color": "purple",
+        "type": "minivan",
+        "registration": new Date('2017-01-03'),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "station wagon",
+        "registration": new Date(),
+        "capacity": 5
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "toyota",
+        "registration": new Date(),
+        "capacity": 6
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7,
+        "notifications": "Yes",
+        "item": ["red", {
+
+            "StudentList": "1st Year Student List",
+            "MeritList": "https://www.sohailuniversity.edu.pk",
+
+
+
+        },
+            {
+                "StudentList": "2nd Year Merit List",
+                "MeritList": "https://www.jmc.edu.pk",
+
+            }]
+
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "vigo",
+        "registration": new Date(),
+        "capacity": 7
+    },
+    {
+        "area": "news",
+        "color": "red",
+        "type": "honda",
+        "registration": new Date(),
+        "capacity": 8
+    },
+
+    {
+        "area": "events",
+        "color": "purple",
+        "type": "minivan",
+        "registration": new Date('2017-01-03'),
+        "capacity": 7
+    },
+    {
+        "area": "exams",
+        "color": "orange",
+        "type": "vigo",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 5
+    },
+    {
+        "area": "exams",
+        "color": "orange",
+        "type": "diahatsu",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 4
+    },
+    {
+        "area": "exams",
+        "color": "orange",
+        "type": "mercedes",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 3
+    },
+    {
+        "area": "exams",
+        "color": "orange",
+        "type": "4 strokes",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 2
+    },
+    {
+        "area": "exams",
+        "color": "orange",
+        "type": "2 strokes",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 5
+    },
+    {
+        "area": "exams",
+        "color": "orange",
+        "type": "euro 2",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 1
+    },
+    {
+        "area": "exams",
+        "color": "orange",
+        "type": "euro",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 5
+    },
+    {
+        "area": "exams",
+        "color": "orange",
+        "type": "euro 2",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 6
+    },
+    {
+        "area": "exams",
+        "color": "orange",
+        "type": "vigo",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 5
+    },
+    {
+        "area": "exams",
+        "color": "orange",
+        "type": "euro",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 4
+    },
+    {
+        "area": "exams",
+        "color": "orange",
+        "type": "4 strokes",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 2
+    },
+    {
+        "area": "exams",
+        "color": "orange",
+        "type": "euro",
+        "registration": new Date(),
+        "notifications": "No",
+        "capacity": 9
+    },
+    {
+        "area": "exams",
+        "color": "orange",
+        "type": "2 strokes",
+        "registration": new Date(),
+        "capacity": 8,
+        "notifications": "Yes",
+        "item": ["orange", {
+
+            "StudentList": "3rd Year Student List",
+            "MeritList": "https://www.sohailuniversity.edu.pk",
+
+
+
+        },
+            {
+                "StudentList": "4th Year Merit List",
+                "MeritList": "https://www.jmc.edu.pk",
+
+            }]
+
+    },
+    {
+        "area": "exams",
+        "color": "orange",
+        "type": "i-vtech",
+        "registration": new Date(),
+        "capacity": 5
+    },
+    {
+        "area": "exams",
+        "color": "orange",
+        "type": "honda",
+        "registration": new Date(),
+        "capacity": 3
+    },
+
+    {
+        "area": "exams",
+        "color": "orange",
+        "type": "minivan",
+        "registration": new Date('2017-01-03'),
+        "capacity": 4
+    },
+    {
+        "area": "admission",
+        "color": "orange",
+        "type": "minivan",
+        "registration": new Date('2017-01-03'),
+        "capacity": 4
+    }
+
+]
+
+
+// var responseText = [{
+//     "area": "CSE",
+//     "cus": "progress",
+//     "project": "Project 1",
+//     "projectdes": "Class    1st"
+// }, {
+//     "area": "ECE",
+//     "cus": "complered",
+//     "project": "Project 2",
+//     "projectdes": "This is class 1st Project"
+// }, {
+//     "area": "IT",
+//     "cus": "progress",
+//     "project": "project 1",
+//     "projectdes": "This is Class 2nd project"
+// }, {
+//     "area": "IT",
+//     "cus": "pending",
+//     "project": "Project 2",
+//     "projectdes": "This is class 2nd project"
+// }];
+
+
+
+
+function showData() {
+
+
+
+    var uniueTabs = getUniqueLists(responseText);
+
+    for (var i = 0; i < uniueTabs.length; i++) {
+
+
+
+
+
+        $('.nav-tabs').append('<li class="list"><a class="alist" href="#tab-content-' + i + '">' + uniueTabs[i].area + '</a></li>');
+        var div = '<div id="tab-content-' + i + '" class="tab-pane fade">';
+
+        for (var j = 0; j < uniueTabs[i].tabContent.length; j++) {
+
+            var obj = uniueTabs[i].tabContent[j];
+            div += '<p>' + obj.registration + obj.color +'</p>';
+            // div += '<p>' + obj.registration + '</p>';
+            
+        }
+
+        $('.tab-content').append(div);
+        $('#tab-content-0').addClass('in active');
+    }
+
+    $('.nav-tabs li').eq(0).addClass('active');
+    setListner();
+
+}
+
+function getUniqueLists(responseText) {
+    var resArr = [];
+
+    responseText.filter(function (x, i) {
+
+        if (resArr.indexOf(x.area) === -1) {
+
+            resArr.push(x.area);
+
+        }
+
+    })
+    return mergeDataAreaWise(resArr, responseText);
+}
+
+function mergeDataAreaWise(area, responseText) {
+
+
+    var tabList = [];
+ 
+
+
+
+    for (var i = 0; i < area.length; i++) {
+        tabList.push({
+            area: area[i],
+            tabContent: []
+        });
+    }
+    for (var i = 0; i < tabList.length; i++) {
+
+
+
+
+        for (var j = 0; j < responseText.length; j++) {
+
+
+            var Obj = {
+
+                color: responseText[j].color,
+                type: responseText[j].type,
+                registration: responseText[j].registration,
+                capacity: responseText[j].capacity
+            }
+            var currentArea = responseText[j].area;
+            if (tabList[i].area === currentArea) {
+                tabList[i].tabContent.push(Obj);
+            }
+        }
+    }
+    console.log(tabList);
+    return tabList;
+}
+
+
+
+
+
+function setListner () {
+
+
+    $(".nav-tabs a").click(function () {
+
+        $(this).tab('show');
+
+
+    });
+
+
+}
